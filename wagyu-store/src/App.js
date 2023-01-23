fa135fc29b711bfbe5828d5023450f3eb462f7ae
per-wat <ifwat.shamsuddin@gmail.com>
2023-01-23 15:47:06 +0800

    Added Homepage

--- wagyu-store/src/App.js
+++ wagyu-store/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ItemCard from "./components/Item Card";
 import NavBar from "./components/NavBar";
+import Homepage from "./components/Homepage";
 import "./styles/App.css"
 import "./styles/variables.css"
 
@@ -9,8 +10,9 @@ function App() {
     <div className="container">
       <Router>
         <Routes>
-          <Route exact path="/" element={<ItemCard />} />
+          <Route exact path="/" element={<Homepage />} />
           <Route path="/navbar" element={<NavBar/>} />
+          <Route path="/itemcard" element={<ItemCard/>} />
         </Routes>
       </Router>
     </div>
---
+++ wagyu-store/src/components/Homepage.js
@@ -0,0 +1,21 @@
+import "../styles/Homepage.css"
+import NavBar from "./NavBar"
+import ItemCard from "./Item Card"
+import data from "./Data Sample.json";
+
+const Homepage = () => {
+
+    return (
+        <div className="container">
+            <NavBar/>
+            <div className="card-container">
+                {Object.values(data).map((item)=>{
+                  return <ItemCard data={item} key={item.cowId}/>
+                })}
+            </div>
+        </div>
+    )
+
+}
+
+export default Homepage
