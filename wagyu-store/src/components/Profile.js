d87b739363313e8f174fe2040a1397766f9a51d0
per-wat <ifwat.shamsuddin@gmail.com>
2023-01-25 01:30:07 +0800

    Configured firestore integration

--- wagyu-store/src/components/Profile.js
+++ wagyu-store/src/components/Profile.js
@@ -1,6 +1,9 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import "../styles/Profile.css";
 import NavBar from "./NavBar";
+import { useAuth } from "./AuthContext";
+import { firestore } from "./Firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import lottie from "lottie-web";
 import { defineElement } from "lord-icon-element";
 <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
@@ -9,19 +12,56 @@ import { defineElement } from "lord-icon-element";
 defineElement(lottie.loadAnimation);
 
 const Profile = () => {
+    const {user} = useAuth();
     const [editing, setEditing] = useState(false);
-    const [address, setAddress] = useState("5405 Jalan Lapangan Terbang Sura Hujung");
-    const [postcode, setPostcode] = useState("23000");
+    const [updating, setUpdating] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [postcode, setPostcode] = useState("");
+    const [contact, setContact] = useState("");
 
-    const handleEdit = useCallback(() => {
+    const getUserInfo = async () => {
+        const userDoc= await getDoc(doc(firestore, "users", user.uid));
+        return(userDoc.data());
+    }
+
+    useEffect(() => {
+        getUserInfo().then((data) => {
+            setName(data.name);
+            setEmail(data.email);
+            setAddress(data.address);
+            setPostcode(data.postcode);
+            setContact(data.contact);
+        });
+        console.log("heh");
+    },[])
+
+    const handleEdit = async () => {
+        if(editing) {
+            try {
+                await updateDoc(doc(firestore, "users", user.uid), {
+                    name: name,
+                    email: email,
+                    address: address,
+                    postcode: postcode,
+                    contact: contact
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        }
         setEditing(!editing);
-    }, [editing])
+    }
+    
 
     const handleChange = e => {
         if (e.target.name === "address") {
             setAddress(e.target.value);
         } else if (e.target.name === "postcode") {
             setPostcode(e.target.value);
+        } else if (e.target.name === "contact") {
+            setContact(e.target.value);
         }
     };
 
@@ -37,12 +77,12 @@ const Profile = () => {
                 <div className="detail-row">
                     <p className="detail-label">Full Name</p>
                     <p className="detail-colon">:</p>
-                    <p className="detail-value">Ifwat Shamsuddin</p>
+                    <p className="detail-value">{name}</p>
                 </div>
                 <div className="detail-row">
                     <p className="detail-label">Email</p>
                     <p className="detail-colon">:</p>
-                    <p className="detail-value">superifwatman@gmail.com</p>
+                    <p className="detail-value">{email}</p>
                 </div>
                 <div className="detail-row pad-top">
                     <p className="detail-label">Address</p>
@@ -74,6 +114,21 @@ const Profile = () => {
                         />
                     ) : (
                         <p className="detail-value">{postcode}</p>
+                    )}
+                </div>
+                <div className="detail-row">
+                    <p className="detail-label">Contact No</p>
+                    <p className="detail-colon">:</p>
+                    {editing ? (
+                        <input
+                            className="detail-value"
+                            type="text"
+                            name="contact"
+                            value={contact}
+                            onChange={handleChange}
+                        />
+                    ) : (
+                        <p className="detail-value">{contact}</p>
                     )}
                 </div>
                 <div className="detail-row">
